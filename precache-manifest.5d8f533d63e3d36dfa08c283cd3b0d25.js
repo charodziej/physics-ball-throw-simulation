896e52b1a51721002c726a0125291b9a302424ba
Adam Jeliński <adas.jelinski@gmail.com>
2019-11-28 19:10:54 +0100

    Updates

---
+++ precache-manifest.5d8f533d63e3d36dfa08c283cd3b0d25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137b4c651e29063d3ada48ae9126b262",
+    "url": "/physics-ball-throw-simulation/index.html"
+  },
+  {
+    "revision": "65751de94dc6708ac513",
+    "url": "/physics-ball-throw-simulation/static/css/main.53e19286.chunk.css"
+  },
+  {
+    "revision": "f4ff2bea9115b917b3ca",
+    "url": "/physics-ball-throw-simulation/static/js/2.e9a3bafc.chunk.js"
+  },
+  {
+    "revision": "65751de94dc6708ac513",
+    "url": "/physics-ball-throw-simulation/static/js/main.ec7f6cdc.chunk.js"
+  },
+  {
+    "revision": "b40a70ed72af9a0bceaa",
+    "url": "/physics-ball-throw-simulation/static/js/runtime-main.6c0621fb.js"
+  }
+]);
