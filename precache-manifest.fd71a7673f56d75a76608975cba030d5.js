e500deb571779c073ecbc4aa97875ae093298e8e
Adam Jeliński <adas.jelinski@gmail.com>
2019-11-28 22:34:46 +0100

    Updates

--- precache-manifest.fd71a7673f56d75a76608975cba030d5.js
+++ precache-manifest.fd71a7673f56d75a76608975cba030d5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2936fec74fb7a636ca7c87187e9c534",
+    "revision": "73751444b0b566419c37e9dcb16753d4",
     "url": "/physics-ball-throw-simulation/index.html"
   },
   {
-    "revision": "2015fe2628f849a988a0",
+    "revision": "c0be8b73eb81cf24e32d",
     "url": "/physics-ball-throw-simulation/static/css/main.53e19286.chunk.css"
   },
   {
-    "revision": "43fcbcf594a782123d0b",
-    "url": "/physics-ball-throw-simulation/static/js/2.7ab27300.chunk.js"
+    "revision": "c8e8bd76830436534cb6",
+    "url": "/physics-ball-throw-simulation/static/js/2.e94d0616.chunk.js"
   },
   {
-    "revision": "2015fe2628f849a988a0",
-    "url": "/physics-ball-throw-simulation/static/js/main.78f50445.chunk.js"
+    "revision": "c0be8b73eb81cf24e32d",
+    "url": "/physics-ball-throw-simulation/static/js/main.030611fc.chunk.js"
   },
   {
     "revision": "b40a70ed72af9a0bceaa",
